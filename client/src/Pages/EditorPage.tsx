3ad6c151cc73f1f183bd68c14e2f2fcd8bdcf79a
Divesh <kumardiv151@gmail.com>
2024-02-09 00:56:11 +0530

    resizable editor panel

--- client/src/Pages/EditorPage.tsx
+++ client/src/Pages/EditorPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import HorizontalTabs from "../components/editor/tabs/HorizontalTabs";
 
 interface Props {}
@@ -6,16 +6,11 @@ interface Props {}
 const EditorPage: React.FC<Props> = () => {
   const editorRef = useRef<HTMLDivElement>(null);
   const sliderRef = useRef<HTMLDivElement>(null);
-  const chatBoxRef = useRef<HTMLDivElement>(null);
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [chatBotWidth, setChatBotWidth] = useState<number | null>(null);
-  const [isResizing, SetIsResizing] = useState<boolean>(false);
 
   const setSliderPosition = () => {
     const editorWidth = editorRef.current!.offsetWidth;
 
-    sliderRef.current!.style.left = `${editorWidth + 1.38}px`;
-    console.log("w : " + editorWidth);
+    sliderRef.current!.style.left = `${editorWidth + 11}px`;
   };
 
   useEffect(() => {
@@ -26,28 +21,28 @@ const EditorPage: React.FC<Props> = () => {
     };
   }, []);
 
-  const handleMouseUp = () => {
-    SetIsResizing(false);
-  };
-
   const handleMouseDown = () => {
-    SetIsResizing(true);
+    // Start resizing immediately on double-click
+    document.addEventListener("mousemove", moveTo);
+
+    // Remove the mousemove event listener when the mouse button is released
+    document.addEventListener("mouseup", handleMouseUp, { once: true });
   };
 
-  const handleResizeOnMove = (e: MouseEvent) => {
-    if (isResizing) {
-      console.log(e.clientX);
-      // const newWidth =
-      //   e.clientX - editorRef.current!.getBoundingClientRect().left;
+  function moveTo(e: MouseEvent) {
+    // e.stopPropagation();
 
-      editorRef.current!.style.width = e.clientX + "px";
-      sliderRef.current!.style.left = `${e.clientX + 1.38}px`;
-    }
+    editorRef.current!.style.width = e.clientX + "px";
+    setSliderPosition();
+  }
+
+  const handleMouseUp = () => {
+    document.removeEventListener("mousemove", moveTo);
   };
 
   return (
     <div
-      className="font fixed h-full max-h-screen w-full bg-backgroundColor-dark"
+      className="font fixed h-full max-h-screen w-full bg-backgroundColor-dark "
       style={{ fontFamily: "Ubuntu" }}
     >
       {/* Header */}
@@ -68,8 +63,11 @@ const EditorPage: React.FC<Props> = () => {
         </ul>
       </div>
 
-      <div className="flex h-full w-full flex-auto gap-2" ref={containerRef}>
-        <div className=" h-full min-w-[60%] max-w-[80%]" ref={editorRef}>
+      <div className="flex h-full w-full flex-auto gap-1 px-2">
+        <div
+          className=" h-full min-w-[60%] max-w-[80%] flex-grow"
+          ref={editorRef}
+        >
           {/* Files names and selection */}
           <div className="flex h-9 w-full items-center justify-between">
             <div className="flex items-center">
@@ -98,26 +96,25 @@ const EditorPage: React.FC<Props> = () => {
               id="ta"
               rows={70}
               cols={95}
+              title="Code editor"
+              placeholder="Enter code here"
               className="h-full w-full bg-overlayDarkColors-dp01 p-4 pt-3 text-textColor-medium outline-none"
             ></textarea>
           </div>
         </div>
 
-        {/* VERTICAL RULER OR SEPERATOR OF CHAT AND CODE */}
-        <div
-          className=" absolute left-10 h-full w-1 cursor-col-resize bg-overlayDarkColors-dp24 hover:bg-blue-700"
-          ref={sliderRef}
-          // onClick={handleCheck}
-          onMouseMove={handleResizeOnMove}
-          onMouseDown={handleMouseDown}
-          onMouseUp={handleMouseUp}
-        ></div>
+        {/* Virtual divider to seperate worksapce*/}
+        <div className={` flex h-full w-[2px] items-center`}>
+          <div
+            className={`absolute top-[50%] h-28 w-[1.5px]  cursor-ew-resize bg-blue-800 px-[1px] opacity-70 
+            `}
+            ref={sliderRef}
+            onMouseDown={handleMouseDown}
+          ></div>
+        </div>
 
         {/* CHAT AREA, FILES SECTION */}
-        <div
-          className=" h-full min-w-[20%] max-w-[40%] bg-overlayDarkColors-dp01"
-          ref={chatBoxRef}
-        ></div>
+        <div className="h-full min-w-[20%] max-w-[40%] flex-grow bg-overlayDarkColors-dp01"></div>
       </div>
     </div>
   );
